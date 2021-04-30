6da373dee69f2605aa3b6033eaf5aa6e25dd72d3
hamzamughal <mughalh473@gmail.com>
2021-04-30 21:37:47 +0500

    database connction added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 
 require('dotenv').config()
 
@@ -26,6 +28,18 @@ app.post(`${api}/products`, (req, res) => {
     res.send(newProduct)
     console.log('====================================');
 })
+
+mongoose.connect(process.env.CONNECTION_STRING,
+    { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        console.log('====================================');
+        console.log(`Database Connection is Ready`);
+        console.log('====================================');
+    }).catch((err) => {
+        console.log('====================================');
+        console.log(err);
+        console.log('====================================');
+    })
 
 app.listen(3000, () => {
     console.log('====================================');

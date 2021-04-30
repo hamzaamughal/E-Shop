06b38a699f597bd72c4f6d2f50853550f5239482
hamzamughal <mughalh473@gmail.com>
2021-04-30 22:04:02 +0500

    added data and retrieve data from database

--- app.js
+++ app.js
@@ -5,28 +5,45 @@ const mongoose = require('mongoose')
 
 
 require('dotenv').config()
+const api = process.env.API_URL
 
 // middleware
 app.use(express.json())
 app.use(morgan('tiny'))
 
-const api = process.env.API_URL
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+const Product = mongoose.model('Product', productSchema)
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find()
+
+    if (!productList) {
+        res.status(500).json({ success: false })
     }
-    res.send(product)
+    res.send(productList)
 })
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log('====================================');
-    console.log(newProduct);
-    res.send(newProduct)
-    console.log('====================================');
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: {
+            type: Number,
+            required: true
+        }
+    })
+    product.save().then((createdProduct) => {
+        res.status(201).json(createdProduct)
+    }).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
 })
 
 mongoose.connect(process.env.CONNECTION_STRING,

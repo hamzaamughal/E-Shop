0c1a93a0ce3504260d003cc673dfd74d17539b8f
hamzamughal <mughalh473@gmail.com>
2021-05-03 12:23:32 +0500

    added password hashing and excluding

--- routers/users.js
+++ routers/users.js
@@ -1,9 +1,10 @@
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 
 router.get(`/`, async (req, res) => {
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if (!userList) {
         res.status(500).json({ success: false })
@@ -11,11 +12,19 @@ router.get(`/`, async (req, res) => {
     res.send(userList);
 })
 
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash')
+    if (!user) {
+        res.status(500).json({ message: 'The User with the given ID was not found' })
+    }
+    res.status(200).send(user)
+})
+
 router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: req.body.passwordHash,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         street: req.body.street,
         isAdmin: req.body.isAdmin,

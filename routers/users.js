783f33aa43c6beb601736739bdec5bcac02e020f
hamzamughal <mughalh473@gmail.com>
2021-05-04 23:00:48 +0500

    users api's part done

--- routers/users.js
+++ routers/users.js
@@ -49,14 +49,60 @@ router.post('/login', async (req, res) => {
     }
     if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign({
-            userId: user.id
-        }, secret,
+            userId: user.id,
+            isAdmin: user.isAdmin
+        }
+            , secret,
             {
                 expiresIn: '1d'
             })
         res.status(200).send({ user: user.email, token: token })
     } else {
         return res.status(400).send('password is wrong!')
+    }
+})
+
+router.post('/register', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        street: req.body.street,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+    user = await user.save()
+    if (!user) {
+        return res.status(400).send('the user cannot be created')
+    }
+    res.send(user)
+})
+
+router.get('/get/count', async (req, res) => {
+    const userCount = await User.countDocuments((count) => count)
+
+    if (!userCount) {
+        res.status(500).json({ success: false })
+    }
+    res.send({
+        userCount: userCount
+    })
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        user = await User.findByIdAndRemove(req.params.id)
+        if (user) {
+            return res.status(200).json({ success: true, message: 'the user is deleted!' })
+        } else {
+            return res.status(400).json({ success: false, message: 'user not found' })
+        }
+    } catch (err) {
+        return res.status(400).json({ success: false, error: err })
     }
 })
 
